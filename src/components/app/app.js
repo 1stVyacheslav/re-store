85560bbd1eade14b7a38eeead53250ee1b639dd7
1stVyacheslav <komaroff4@gmail.com>
2019-07-29 13:22:32 +0500

    add shop-header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './app.css';
 
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import { HomePage, CartPage } from '../pages';
+import ShopHeader from '../shop-header/shop-header';
 
 
 const App = () => {
@@ -15,10 +16,12 @@ const App = () => {
 	return (
 			<main role='main' className='container'>
 
-				<Link to='/' className='btn btn-primary mr-3'>Home Page</Link>
+				<ShopHeader numItems={2} total={200}/>
+
+				{/* <Link to='/' className='btn btn-primary mr-3'>Home Page</Link>
 				<Link to='/cart' className='btn btn-primary mr-3'>Cart Page</Link>
 				<Link to='/spinner' className='btn btn-primary mr-3'>spinner</Link>
-				<Link to='/error' className='btn btn-primary mr-3'>error</Link>				
+				<Link to='/error' className='btn btn-primary mr-3'>error</Link>				 */}
 
 				<Switch>
 
---
+++ src/components/shop-header/shop-header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './shop-header.css';
+
+const ShopHeader = ( { numItems, total } ) => {
+
+	return (
+		<header className="shop-header row">
+			<div href='#' className='logo text-dark'>ReStore</div>
+			<div className='shopping-cart'>
+				<i className='cart-icon fa fa-shopping-cart' />
+				{numItems} items (${total})
+			</div>
+		</header>
+	)
+}
+
+export default ShopHeader;
